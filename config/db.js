8272fba1289f8e19c0f14c50f43eb9642703436b
Arlyn0603 <162391993+Arlyn0603@users.noreply.github.com>
2025-04-24 17:27:18 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const sql = require("mssql");
 
 const config = {
-    user: "EventosDB", // Nombre de usuario proporcionado en Plesk
-    password: "EventosDB1.", // Coloca aquí la contraseña que se muestra en Plesk (no se ve en la imagen)
-    server: "tiusr16pl.cuc-carrera-ti.ac.cr", // Host de la base de datos en Plesk
+    user: "EventosDB",
+    password: "EventosDB1.",
+    server: "tiusr16pl.cuc-carrera-ti.ac.cr",
     database: "EventosDB",
+    port: 1433, // Explicitly add this
     options: {
-        encrypt: true, // Importante activar para conexiones remotas
-        trustServerCertificate: true // Esto depende de si el certificado es confiable o no
+        encrypt: true,
+        trustServerCertificate: true
     }
 };
 
